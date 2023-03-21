3ac3afbf2127dbbc85917269e543e7db7315d060
aloksinghse <se.aloksingh@gmail.com>
2023-03-21 09:05:27 +0530

    error fix

--- src/components/pages/Home/ContactUs.tsx
+++ src/components/pages/Home/ContactUs.tsx
@@ -46,10 +46,7 @@ const ContactUs = () => {
           <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
             <form action="#" className="space-y-8">
               <div>
-                <label
-                  for="email"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                >
+                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
                   Your email
                 </label>
                 <input
@@ -63,10 +60,7 @@ const ContactUs = () => {
                 />
               </div>
               <div>
-                <label
-                  for="subject"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                >
+                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
                   Subject
                 </label>
                 <input
@@ -80,15 +74,12 @@ const ContactUs = () => {
                 />
               </div>
               <div className="sm:col-span-2">
-                <label
-                  for="message"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
-                >
+                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">
                   Your message
                 </label>
                 <textarea
                   id="message"
-                  rows="6"
+                  rows={6}
                   className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                   placeholder="Leave a comment..."
                   required
